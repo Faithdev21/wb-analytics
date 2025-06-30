55eab47aad45b95343eb4cbb9eb265a20bbb5477
egor <egor@mail.ru>
2025-06-30 17:39:25 +0300

    mvp ready

--- backend/wildberries/api/filters.py
+++ backend/wildberries/api/filters.py
@@ -1,4 +1,6 @@
 from django_filters.rest_framework import FilterSet, NumberFilter
+from rest_framework.exceptions import ValidationError
+
 from products.models import Product
 
 
@@ -10,4 +12,26 @@ class ProductFilter(FilterSet):
 
     class Meta:
         model = Product
-        fields = []
+        fields = ['min_price', 'max_price', 'min_rating', 'min_reviews']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        query_params = self.request.query_params
+        errors = {}
+
+        try:
+            min_rating = float(query_params.get("min_rating", 0))
+            if not (0 <= min_rating <= 5):
+                errors["min_rating"] = ["Рейтинг должен быть от 0 до 5"]
+        except ValueError:
+            errors["min_rating"] = ["Некорректное значение рейтинга"]
+
+        try:
+            min_reviews = int(query_params.get("min_reviews", 0))
+            if min_reviews < 0:
+                errors["min_reviews"] = ["Количество отзывов не может быть < 0"]
+        except ValueError:
+            errors["min_reviews"] = ["Некорректное значение числа отзывов"]
+
+        if errors:
+            raise ValidationError(errors)
--- backend/wildberries/wildberries/settings.py
+++ backend/wildberries/wildberries/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'wildberries.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
